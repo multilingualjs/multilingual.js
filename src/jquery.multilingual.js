003580bcc01a65249210511fe051d20dc96a15d3
bizarre <b@tumblbug.com>
2016-04-04 12:39:17 -0400

    &amp; &lt; &gt; 등 html 이스케이프 문자가 커스텀 세팅 및 구두점에서 제대로 잡히도록 수정

--- src/jquery.multilingual.js
+++ src/jquery.multilingual.js
@@ -5,7 +5,7 @@
     jp: "[\u3040-\u309F\u30A0-\u30FF]+",
     cn: "[\u4E00-\u9FBF]+",
     num: "[0-9]+",
-    punct: "[\(\).,“”\-]+"
+    punct: "[\(\).,“”\-]|&amp;|&lt;|&gt;+"
   }
 
   function MultiLingual(params){
@@ -43,6 +43,42 @@
 
     },
     
+    escape_regex_str: function(str) {
+      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    },
+
+    compute_custom_regex: function (charset) {
+      
+      var html_escaped_chars = [];
+      var final_str = "";
+
+      if (charset.match(/\&/) != null || charset.match(/\&amp;/) != null){
+        html_escaped_chars.push("&amp;");
+        charset = charset.replace(/\&/, "");
+        charset = charset.replace(/\&amp;/, "");
+      }
+
+      if (charset.match(/</) != null || charset.match(/\&lt;/) != null){
+        html_escaped_chars.push("&lt;");
+        charset = charset.replace(/</, "");
+        charset = charset.replace(/\&lt;/, "");
+      }
+
+      if (charset.match(/>/) != null || charset.match(/\&gt;/) != null){
+        html_escaped_chars.push("&gt;");
+        charset = charset.replace(/>/, "");
+        charset = charset.replace(/\&gt;/, "");
+      }
+
+
+      if (html_escaped_chars.length > 0) {
+        final_str = "([" + charset + "]|" + html_escaped_chars.join("|") + "+)";
+      } else {
+        final_str = "([" + charset + "]+)";
+      }
+      return final_str;
+    },
+
 
     compose_regex: function(){
       var final_regex_str = "(\?![^<>&]*>)";
@@ -53,7 +89,7 @@
         if (typeof config == "string"){
           final_regex_str += "(" + regexs[config] + ")";
         } else {
-          final_regex_str += "([" + config.charset + "]+)";
+          final_regex_str += this.compute_custom_regex(config.charset);
         }
         
         if (i < this.configuration.length - 1) {
